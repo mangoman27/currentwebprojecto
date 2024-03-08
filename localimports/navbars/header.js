452f684bb9048f84d8b1f0dab276044bc0ba874d
bob <103124082+mangoman27@users.noreply.github.com>
2024-03-08 22:14:51 +0000

    Add files via upload

---
+++ localimports/navbars/header.js
@@ -0,0 +1,67 @@
+class Header extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+      
+		
+
+	  
+	  	<div class="header">
+	<div class="headerlinks">
+	
+	
+		  	  <a  class="a2" >Pages:</a>
+
+	  <a  class="a"  href="/index.html">Home</a>
+	  <a  class="a"  href="/blog">Blog</a>
+	  <a  class="a"  href="/gallery/index.html">Gallery</a>
+	  <a  style="float:right" class="a2 bob"> Site Updated: 03/03/24 </a>
+
+			  
+	
+	
+	
+	</div>
+	</div>
+
+	
+		
+
+	
+	
+	<br>
+
+	  
+	  
+	  
+
+
+
+	  
+	  
+	  
+
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+    `;
+  }
+}
+
+customElements.define('header-component', Header);
+
